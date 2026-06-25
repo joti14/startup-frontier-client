910e88a786beacd1dc3d15ff7cf0210d92bfd8b7
Joti <hasnahenajoti1414@gmail.com>
2026-06-25 23:49:38 +0600

    baseurl

--- src/lib/api/baseUrl.js
+++ src/lib/api/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+export const baseURL = process.env.NEXT_PUBLIC_API_URL || (process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000');
